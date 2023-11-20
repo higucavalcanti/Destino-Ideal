d1f2ec570485ef23ffe7038c3da035f33a5a59b0
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-11-20 11:42:05 -0300

    Estilização inicial do Front
    App.js e Header

---
+++ frontend/src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import * as C from './styles';
+
+export const Header = () => {
+  return (
+    <C.Container>
+        <C.Logo></C.Logo>
+            <p>Destino Ideal</p>
+    </C.Container>
+
+  )
+}
