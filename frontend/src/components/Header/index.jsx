071f00cef2dc78f549a82c1e3d45bb59ff5878f1
higucavalcanti <cavalcanti.higor1@gmail.com>
2023-11-21 22:00:35 -0300

    Inserindo logo no Header

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,11 +1,12 @@
 import * as C from './styles';
+import logo  from '../../assets/logo.png'
+
 
 export const Header = () => {
   return (
     <C.Container>
-        <C.Logo></C.Logo>
-            <p>Destino Ideal</p>
+      <C.Logo><a href="/"> <img src={logo} alt="" /></a></C.Logo>
+      <h1>Destino Ideal</h1>
     </C.Container>
-
   )
 }
--- frontend/src/components/Header/styles.js
+++ frontend/src/components/Header/styles.js
@@ -3,12 +3,8 @@ import styled from "styled-components";
 export const Container = styled.div`
     background: #F9F9F9;
     flex: 1;
-    flex: row;
-    flex: justify-center;
-    items aling: center: p-1;
     padding: 10px 0;
-    boder-bottom: 1px solid #16195C;
-    
+
     p {
         font-family: 'Inter';
         font-size: 25px;
@@ -20,9 +16,12 @@ export const Logo = styled.div`
     width: 60px;
     height: 60px;
     border-radius: 50%;
-    background-color: #191A59;
     display: flex;
     justify-content: center;
     align-items: center;
-    font-size: 25px;
+
+    a {
+        display: flex;
+        align-items: center;
+    }
 `;
