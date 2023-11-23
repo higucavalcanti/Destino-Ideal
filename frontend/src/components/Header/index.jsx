2b1d62fa0b93e7bdb5884c613a338c91f95fb7a9
higucavalcanti <cavalcanti.higor1@gmail.com>
2023-11-23 15:34:05 -0300

    Componentizando Logo

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -4,9 +4,7 @@ import logo  from '../../assets/logo.png'
 export const Header = () => {
   return (
     <C.Container>
-      <C.Logo>
-        <img src={logo} alt="DI logo" />
-      </C.Logo>
+      <C.Logo src={logo} alt="DI logo" />
       <p>Destino Ideal</p>
     </C.Container>
   )
