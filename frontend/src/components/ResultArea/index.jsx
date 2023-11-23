d89e24342abfd65e5d973544979d9b4b40a23c87
higucavalcanti <cavalcanti.higor1@gmail.com>
2023-11-23 19:18:02 -0300

    Iniciando ResultArea

--- frontend/src/components/ResultArea/index.jsx
+++ frontend/src/components/ResultArea/index.jsx
@@ -3,7 +3,8 @@ import * as C from './styles';
 export const ResultArea = () => {
     return (
       <C.Container>
-        ...
+        <h1>Resultado</h1>
+        <C.ResultContainer></C.ResultContainer>
       </C.Container>
     )
   }
--- frontend/src/components/ResultArea/styles.js
+++ frontend/src/components/ResultArea/styles.js
@@ -1,4 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    padding: 4rem;
+    background: #FFF;
+
+    h1 {
+        font-family: 'Inter';
+        text-align: center;
+    }
+`;
+
+export const ResultContainer = styled.div`
 `;
