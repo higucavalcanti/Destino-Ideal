78910fb3298676156a86390fe833a4bc6a43ddd3
higucavalcanti <cavalcanti.higor1@gmail.com>
2023-11-23 17:00:00 -0300

    Tentando printar texto na tela (já com input e icone)

--- frontend/src/components/SearchArea/index.jsx
+++ frontend/src/components/SearchArea/index.jsx
@@ -1,13 +1,23 @@
+import React from 'react';
 import * as C from './styles';
 import video from '../../assets/video.mp4';
+import icone from '../../assets/icone.png';
 
 export const SearchArea = () => {
-    return (
-      <C.Container>
-        <div className="overlay"></div>
-        <C.Video src={video} muted autoPlay loop type="video/mp4" />
-            <h1>Pesquise seu destino</h1>
-            <p>e saiba mais sobre ele</p>
-      </C.Container>
-    )
-  }
+  return (
+    <C.Container>
+      <div className="overlay"></div>
+      <C.Video src={video} muted autoPlay loop type="video/mp4" />
+      <C.Content>
+        <h1>Pesquise seu destino</h1>
+        <p>e saiba mais sobre ele</p>
+        <label>
+          <input className="input"
+          type="text" 
+          placeholder="Pesquise seu destino" />
+        <C.Icon src={icone} alt="icone de pesquisa"/>
+        </label>
+      </C.Content>
+    </C.Container>
+  );
+};
--- frontend/src/components/SearchArea/styles.js
+++ frontend/src/components/SearchArea/styles.js
@@ -1,32 +1,60 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    padding: 4rem 0 2rem;
     position: relative;
     width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    object-fit: contain;
+    text-align: center;
+    color: #fff;
+    padding: 150px;
 
-    .overlay{
+    .overlay {
         position: absolute;
         height: 100%;
         width: 100%;
-        background: ..........
         top: 0;
         bottom: 0;
         z-index: 1;
         mix-blend-mode: hard-light;
     }
-
-    .h1{
-        color: #000;
-        font-size: 50px;
-        margin-top: -100px
-    }
+`;
     
+export const Content = styled.div`
+    z-index: 2;
+
+    h1 {
+        font-size: 150px;
+        margin-top: -100px;
+    }
+
+    p {
+        margin-top: 10px;
+        font-size: 45px;
+    }
+
+    label {
+        margin-top: 20px;
+        display: flex;
+        align-items: center;
+        width: 80%;
+
+        .input {
+            flex: 1;
+            padding: 10px;
+            border: 2px solid #fff;
+            border-radius: 10px;
+            margin-right: 10px;
+            color: #000;
+        }
+    }
+`;
+
+export const Icon = styled.div`
+    width: 24px;
+    height: 24px;
 `;
 
 export const Video = styled.video`
