3c834d15a5dc06c243f086421db1176716edaeec
higucavalcanti <cavalcanti.higor1@gmail.com>
2023-11-23 15:32:44 -0300

    Criando GlobalStyle e importando no App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
+import React from 'react';
 import * as C from './App.styles';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { SearchArea } from './components/SearchArea';
 
 const App = () => {
-
   return (
-    <C.Container>
-      <Header/>
-      <SearchArea/>
-      <Footer/>
-    </C.Container>
+    <>
+      <C.GlobalStyle />
+      <Header />
+      <SearchArea />
+      {/*ResultArea */}
+      <Footer />
+    </>
   );
 }
 
--- frontend/src/App.styles.js
+++ frontend/src/App.styles.js
@@ -1,11 +1,20 @@
-import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
 {/* https://www.figma.com/file/ZHmcX7pf9RartgeXbOD4Ci/Untitled?type=design&node-id=1-2&mode=design*/}
 
-export const Container = styled.div`
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
     background-color: #FFF;
     color: #000;
+    font-family: 'Inter', sans-serif;
     min-height: 100vh;
     display: flex;
     flex-direction: column;
+  }
 `;
