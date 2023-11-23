61d8b3741c28ebc9e7f1f592430497d32b1cd932
higucavalcanti <cavalcanti.higor1@gmail.com>
2023-11-23 15:37:32 -0300

    Componentizando Video e adicionando frases

--- frontend/src/components/SearchArea/index.jsx
+++ frontend/src/components/SearchArea/index.jsx
@@ -4,7 +4,10 @@ import video from '../../assets/video.mp4';
 export const SearchArea = () => {
     return (
       <C.Container>
-        ...
+        <div className="overlay"></div>
+        <C.Video src={video} muted autoPlay loop type="video/mp4" />
+            <h1>Pesquise seu destino</h1>
+            <p>e saiba mais sobre ele</p>
       </C.Container>
     )
   }
