f3398ce01605b07fa410431602c84a0bf4d74cde
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-11-24 08:28:00 -0300

    sinalizando onde colocar os codigos dos Resultados

--- frontend/src/components/ResultArea/index.jsx
+++ frontend/src/components/ResultArea/index.jsx
@@ -4,7 +4,10 @@ export const ResultArea = () => {
     return (
       <C.Container>
         <h1>Resultado</h1>
-        <C.ResultContainer></C.ResultContainer>
+        <C.ResultContainer>
+          {/* aqui coloca o espaço e o que vai receber 
+            como reultado da pesquisa do Destino */}
+        </C.ResultContainer>
       </C.Container>
     )
   }
