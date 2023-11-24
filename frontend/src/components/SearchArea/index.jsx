ed2c0324f281b0aa22a475983cd4eedbbcd749fc
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-11-24 08:28:43 -0300

    função para clicar Enter na barra de Pesquisa

--- frontend/src/components/SearchArea/index.jsx
+++ frontend/src/components/SearchArea/index.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as C from './styles';
 import video from '../../assets/video.mp4';
 import icone from '../../assets/icone.png';
 
 export const SearchArea = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      console.log('Pesquisar:', searchText);
+
+      setSearchText('');
+    }
+  };
+
   return (
     <C.Container>
       <div className="overlay"></div>
@@ -12,8 +22,14 @@ export const SearchArea = () => {
         <h1>Pesquise seu destino</h1>
         <p>e saiba mais sobre ele</p>
         <div className="input flex">
-          <input type="text" placeholder="Pesquise seu destino" />
-          <C.Icon src={icone} alt="icone de pesquisa"/>
+          <input
+            type="text"
+            placeholder="Pesquise seu destino"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+          <C.Icon src={icone} alt="icone de pesquisa" />
         </div>
       </C.Content>
     </C.Container>
