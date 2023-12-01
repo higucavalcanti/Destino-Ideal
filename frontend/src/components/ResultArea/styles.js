4753736fe0121b25766d4ecc9a13898a05fdb2b5
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-12-01 18:17:08 -0300

    Refatorando ResultArea e Deletando Pages
    - Deletei o Parent Component de distribui o que ele fazia no App.js
    - Importei tudo no App.js
    - ReEstilizando para deixar o mais proximo do que tava antes

--- frontend/src/components/ResultArea/styles.js
+++ frontend/src/components/ResultArea/styles.js
@@ -1,42 +1,17 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    padding: 4rem;
-    background: #FFF;
-
-    h1 {
-        font-family: 'Inter';
-        text-align: center;
-        font-size: 45px;
-    }
+    padding: 64px 64px 0;
 `;
 
 export const ResultContainer = styled.div`
     background-color: #F5F5F5;
-    margin-top: 10px;
-    border-radius: 20px;
+    font-family: 'Inter';
+    font-size: 25px;
+    display: block;
+    color: #26897D;
+
+    border-radius: 20px 20px 0 0;
     padding: 20px;
-    margin-top: 50px;
-
-
-    label {
-        font-family: 'Inter';
-        color: #26897D;
-        font-size: 25px;
-        display: block;
-        margin-bottom: 20px;
-
-        textarea {
-            background-color: #F5F5F5;
-            display: block;
-            margin-top: 10px;
-            box-sizing: border-box;
-            width: 100%;
-            border: none;
-            border-radius: 10px;
-            color: #000;
-            outline: none;
-            font-size: 15px;
-        }
-    }
+    margin-top: 100px;
 `;
