bb9970f22c99f7e7f8fc922caefbb2a4d04359c6
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-12-01 22:47:05 -0300

    Adicionando de click "Enter" (texto some)

--- frontend/src/components/ResultArea/index.jsx
+++ frontend/src/components/ResultArea/index.jsx
@@ -4,6 +4,11 @@ import * as C from './styles';
 export const ResultArea = () => {
   const [searchPoint, setSearchPoint] = useState('');
 
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+    }
+  };
+
   return (
     <C.Container>
       <C.ResultContainer>Resultado</C.ResultContainer>
@@ -13,6 +18,7 @@ export const ResultArea = () => {
               type="text"
               placeholder="Pesquise um ponto (Restaurante, hoteis e etc...)"
               value={searchPoint}
+              onKeyDown={handleKeyDown}
           />
         </div>
       </C.ResultInput>
