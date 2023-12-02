be4610ada3161643a044a35f18e82784bd5cefff
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-12-01 22:45:36 -0300

    Criando ResultInput

--- frontend/src/components/ResultArea/index.jsx
+++ frontend/src/components/ResultArea/index.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as C from './styles';
 
 export const ResultArea = () => {
+  const [searchPoint, setSearchPoint] = useState('');
+
   return (
     <C.Container>
-      <C.ResultContainer>
-          Resultado
-      </C.ResultContainer>
+      <C.ResultContainer>Resultado</C.ResultContainer>
+      <C.ResultInput>
+        <div className="input flex">
+          <input
+              type="text"
+              placeholder="Pesquise um ponto (Restaurante, hoteis e etc...)"
+              value={searchPoint}
+          />
+        </div>
+      </C.ResultInput>
     </C.Container>
   );
 };
--- frontend/src/components/ResultArea/styles.js
+++ frontend/src/components/ResultArea/styles.js
@@ -1,17 +1,42 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    background-color: #F00000;
     padding: 64px 64px 0;
+    display: flex;
 `;
 
 export const ResultContainer = styled.div`
     background-color: #F5F5F5;
     font-family: 'Inter';
     font-size: 25px;
-    display: block;
     color: #26897D;
-
-    border-radius: 20px 20px 0 0;
+    width: 100%;
+    border-radius: 20px 0 0 0;
     padding: 20px;
     margin-top: 100px;
+`;
+
+export const ResultInput = styled.div`
+  display: flex;
+  margin-top: 100px;
+  padding: 10px;
+  align-items: center;
+  border-radius: 0 20px 0 0;
+  background-color: #000;
+
+  input {
+    padding: 10px;
+    border-radius: 3rem;
+    border: none;
+    outline: none;
+    background: #FFF;
+    width: 345px;
+    font-size: 16px;
+    font-weight: 500;
+    margin-left: auto;
+    &::placeholder {
+      font-size: 15px;
+    }
+  }
 `;
